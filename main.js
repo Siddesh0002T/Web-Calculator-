175c016a912a5efacc6369f22cf605d3735cc553
Siddhesh Ravindra More <94625517+Siddesh0002T@users.noreply.github.com>
2024-06-01 08:32:04 +0530

    Project Upload
    ADD project

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log('Hello World!');
+function git() {
+  window.location = 'https://github.com/Siddesh0002T';
+}
+function insta() {
+  window.location = 'https://www.instagram.com/Siddhesh0002t';
+}
+function linkedIn(){
+  window.location = 'https://www.linkedin.com/in/siddhesh0002t?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app';
+}
